c2e0eeb4857bac60510a2b98d22e3decc1d9f278
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2022-03-16 20:50:05 +0900

    feat(WAT): Add `AddIntIndex` function

---
+++ wat/AddIntIndex.js
@@ -0,0 +1,11 @@
+const fs    = require('fs');
+const bytes = fs.readFileSync(`${__dirname}/AddIntIndex.wasm`);
+
+const v1 = Number(process.argv[2]);
+const v2 = Number(process.argv[3]);
+
+WebAssembly
+  .instantiate(new Uint8Array(bytes))
+  .then(({ instance }) => {
+    console.log(`${v1} + ${v2} = ${instance.exports.AddInt(v1, v2)}`);
+  });
