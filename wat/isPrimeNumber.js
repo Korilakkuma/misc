94c6cdf3785bda077111ea33666860c578f3a5a3
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2022-03-24 19:11:31 +0900

    feat(WAT): Add `isPrimeNumber` function

---
+++ wat/isPrimeNumber.js
@@ -0,0 +1,14 @@
+const fs    = require('fs');
+const bytes = fs.readFileSync(`${__dirname}/isPrimeNumber.wasm`);
+
+const n = Number(process.argv[2]);
+
+WebAssembly
+  .instantiate(new Uint8Array(bytes))
+  .then(({ instance }) => {
+    if (instance.exports.isPrimeNumber(n)) {
+      console.log(`${n} is prime number`);
+    } else {
+      console.log(`${n} is not prime number`);
+    }
+  });
