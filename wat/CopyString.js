8ab46a834c71cbf6e78dfeb82b46434d1e248d7c
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2022-04-11 03:51:02 +0900

    feat(WAT): Add `CopyString` function

---
+++ wat/CopyString.js
@@ -0,0 +1,11 @@
+const fs    = require('fs');
+const bytes = fs.readFileSync(`${__dirname}/CopyString.wasm`);
+
+const importObject = {
+};
+
+WebAssembly
+  .instantiate(new Uint8Array(bytes), importObject)
+  .then(({ instance }) => {
+    // TODO;
+  });
