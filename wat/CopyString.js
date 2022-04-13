2d5f6446087ccb560015e83809d864502fb4cf97
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2022-04-13 20:47:45 +0900

    feat(WAT): Add function for showing string by using prefix

--- wat/CopyString.js
+++ wat/CopyString.js
@@ -20,6 +20,13 @@ const importObject = {
       const string = new TextDecoder('utf8').decode(bytes).split('\0')[0];
 
       console.log(string);
+    },
+    strPrefix: (pos) => {
+      const length = new Uint8Array(memory.buffer, pos, 1)[0];
+      const bytes  = new Uint8Array(memory.buffer, (pos + 1), length);
+      const string = new TextDecoder('utf8').decode(bytes);
+
+      console.log(string);
     }
   }
 };
