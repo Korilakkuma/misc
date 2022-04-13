913f8f0d77f9781a8b2d98ebfa6c3b6d4a1d3771
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2022-04-13 19:58:58 +0900

    feat(WAT): Add function for showing string by assigning position and length

--- wat/CopyString.js
+++ wat/CopyString.js
@@ -2,11 +2,22 @@ const fs = require('fs');
 
 const bytes = fs.readFileSync(`${__dirname}/CopyString.wasm`);
 
+const memory = new WebAssembly.Memory({ initial: 1 });
+
 const importObject = {
+  env: {
+    buffer   : memory,
+    strPosLen: (pos, len) => {
+      const bytes  = new Uint8Array(memory.buffer, pos, len);
+      const string = new TextDecoder('utf8').decode(bytes);
+
+      console.log(string);
+    }
+  }
 };
 
 WebAssembly
   .instantiate(new Uint8Array(bytes), importObject)
   .then(({ instance }) => {
-    // TODO;
+    instance.exports.main();
   });
