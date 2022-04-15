c4b0f24fcabf11adb29e95814957d7d4ae3257c1
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2022-04-15 19:39:18 +0900

    feat(WAT): Add function for `toString(10)`

---
+++ wat/toString.js
@@ -0,0 +1,24 @@
+const fs    = require('fs');
+const bytes = fs.readFileSync(`${__dirname}/toString.wasm`);
+
+const num = Number(process.argv[2]);
+
+const memory = new WebAssembly.Memory({ initial: 1 });
+
+const importObject = {
+  env: {
+    buffer     : memory,
+    printString: (pos, len) => {
+      const b = new Uint8Array(memory.buffer, pos, len);
+      const s = new TextDecoder('utf8').decode(b);
+
+      console.log(`>${s}!`);
+    }
+  }
+};
+
+WebAssembly
+  .instantiate(new Uint8Array(bytes), importObject)
+  .then(({ instance }) => {
+    instance.exports.toString10(num);
+  });
