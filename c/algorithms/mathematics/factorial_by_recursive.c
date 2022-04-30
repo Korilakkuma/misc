9e9745621d742f2ab9b06e1a51d801c80aff8803
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2022-04-30 23:53:19 +0900

    feat(algorithms): factorial by recursive

---
+++ c/algorithms/mathematics/factorial_by_recursive.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+static long long factorial(long long n);
+
+int main(int argc, char **argv) {
+  if (argc != 2) {
+    fputs("./factorial_by_recursive [natural number]", stderr);
+    exit(EXIT_FAILURE);
+  }
+
+  long long n = strtol(argv[1], NULL, 10);
+  long long r = factorial(n);
+
+  printf("%lld! = %lld\n", n, r);
+
+  return 0;
+}
+
+long long factorial(long long n) {
+  if (n == 1) {
+    return 1;
+  }
+
+  return n * factorial(n - 1);
+}
