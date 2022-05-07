f763454226b664950508c8216d0066514ea82300
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2022-05-07 20:25:25 +0900

    feat(algorithms): Newton method

---
+++ c/algorithms/mathematics/newton_method.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+static double quadratic_function(double n, long long c);
+static double cubic_function(double n, long long c);
+static double exponential(double n, long long c);
+
+int main(int argc, char **argv) {
+  if (argc != 3) {
+    fputs("./newton_method [natural number] [natural number]\n", stderr);
+    exit(EXIT_FAILURE);
+  }
+
+  double n    = strtod(argv[1], NULL);
+  long long c = strtoll(argv[2], NULL, 10);
+
+  printf("x^2 = %f\n", quadratic_function(n, c));
+  printf("x^3 = %f\n", cubic_function(n, c));
+  printf("e^x = %f\n", exponential(n, c));
+
+  return 0;
+}
+
+static double quadratic_function(double n, long long c) {
+  double a = n;
+
+  for (long long i = 0; i < c; i++) {
+    double x = a;
+    double y = pow(x, 2.0);
+
+    double dc = 2.0 * pow(x, 1.0);
+    double ct = y - (dc * x);
+
+    a = (n - ct) / dc;
+  }
+
+  return a;
+}
+
+static double cubic_function(double n, long long c) {
+  double a = n;
+
+  for (long long i = 0; i < c; i++) {
+    double x = a;
+    double y = pow(x, 3.0);
+
+    double dc = 3.0 * pow(x, 2.0);
+    double ct = y - (dc * x);
+
+    a = (n - ct) / dc;
+  }
+
+  return a;
+}
+
+static double exponential(double n, long long c) {
+  double a = n;
+
+  for (long long i = 0; i < c; i++) {
+    double x = a;
+    double y = exp(x);
+
+    double dc = exp(x);
+    double ct = y - (dc * x);
+
+    a = (n - ct) / dc;
+  }
+
+  return a;
+}
