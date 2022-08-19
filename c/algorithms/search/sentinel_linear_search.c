e70f124b32f78ede2b2d5a9b1f1b9a17d4312f23
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2022-08-19 23:58:33 +0900

    feat(algorithms): Sentinel Linear Search

---
+++ c/algorithms/search/sentinel_linear_search.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define INT long long
+#define N 10
+#define RANGE 10
+
+int main(int argc, char **argv) {
+  if (argc != 2) {
+    fputs("./sentinel_linear_search [0-9]\n", stderr);
+    exit(EXIT_FAILURE);
+  }
+
+  INT key = strtoll(argv[1], NULL, 10);
+
+  INT a[N + 1];
+
+  a[N] = key;
+
+  srand((unsigned int)time(NULL));
+
+  for (INT i = 0; i < N; i++) {
+    a[i] = RANGE * ((double)rand() / (RAND_MAX + 1.0));
+  }
+
+  INT i = 0;
+
+  while (a[i] != key) {
+    i++;
+  }
+
+  if (i < N) {
+    printf("%lld is found (a[%lld])\n\n", a[i], i);
+  } else {
+    printf("%lld is not found\n\n", key);
+  }
+
+  printf("dump\n");
+
+  for (INT i = 0; i < N + 1; i++) {
+    printf("a[%lld] = %lld\n", i, a[i]);
+  }
+
+  printf("\n");
+
+  return 0;
+}
