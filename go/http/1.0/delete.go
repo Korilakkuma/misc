0f527e066348accc4ee0ec9819bffd9901e0ad5e
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2023-01-19 23:32:41 +0900

    feat(HTTP): DELETE client (`form` does not support HTTP method)

---
+++ go/http/1.0/delete.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"net/http/httputil"
+)
+
+func main() {
+	client := &http.Client{}
+
+	req, err := http.NewRequest("DELETE", "http://localhost:18888", nil)
+
+	if err != nil {
+		panic(err)
+	}
+
+	res, err := client.Do(req)
+
+	if err != nil {
+		panic(err)
+	}
+
+	dump, err := httputil.DumpResponse(res, true)
+
+	log.Println(string(dump))
+}
