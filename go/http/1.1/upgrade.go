c92c02ec55acf6178bae9d14202d7827dfe25280
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2023-01-30 23:45:27 +0900

    feat(HTTP): Protocol Upgrade

---
+++ go/http/1.1/upgrade.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"net/http"
+	"time"
+)
+
+func main() {
+	dialer := &net.Dialer{
+		Timeout:   30 * time.Second,
+		KeepAlive: 30 * time.Second,
+	}
+
+	conn, err := dialer.Dial("tcp", "localhost:18888")
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer conn.Close()
+
+	reader := bufio.NewReader(conn)
+
+	request, _ := http.NewRequest("GET", "http://localhost:18888/upgrade", nil)
+	request.Header.Set("Connection", "Upgrade")
+	request.Header.Set("Upgrade", "TestProtocol")
+
+	err = request.Write(conn)
+
+	if err != nil {
+		panic(err)
+	}
+
+	res, err := http.ReadResponse(reader, request)
+
+	if err != nil {
+		panic(err)
+	}
+
+	log.Println("Status: ", res.Status)
+	log.Println("Headers: ", res.Header)
+
+	counter := 10
+
+	for {
+		data, err := reader.ReadBytes('\n')
+
+		if err == io.EOF {
+			break
+		}
+
+		fmt.Println("<-", string(bytes.TrimSpace(data)))
+		fmt.Fprintf(conn, "%d\n", counter)
+		fmt.Println("->", counter)
+		counter--
+	}
+}
