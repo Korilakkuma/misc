56e781443be8cd75bc91a550f1d8017458068294
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2023-10-16 21:20:55 +0900

    feat(WebAssembly): Fourier series (sawtooth)

--- c/webassembly/fourier-series/fourier-series.c
+++ c/webassembly/fourier-series/fourier-series.c
@@ -37,3 +37,29 @@ float *square(const int N, const int K) {
 
   return fourier_series;
 }
+
+#ifdef __EMSCRIPTEN__
+EMSCRIPTEN_KEEPALIVE
+#endif
+float *sawtooth(const int N, const int K) {
+  if (fourier_series) {
+    free(fourier_series);
+  }
+
+  fourier_series = (float *)calloc(N, sizeof(float));
+
+  for (int n = 0; n < N; n++) {
+    fourier_series[n] = 0.0f;
+
+    for (int k = 1; k < K; k++) {
+      float x = n / RESOLUTION;
+
+      // f(x) = 4x (-M_PI < x < M_PI)
+      //
+      // += (1 / 4) * (2 * ((-1)^(k + 1) / k) * sinkx)
+      fourier_series[n] += (1.0f / 4.0f) * (2.0f * (powf(-1.0f, (float)(k + 1)) / k) * sinf((float)k * x));
+    }
+  }
+
+  return fourier_series;
+}
