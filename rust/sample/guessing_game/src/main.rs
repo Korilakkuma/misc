fcf85cb633b6b032b71f60f8571e75e43d44110f
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2023-11-14 23:38:45 +0900

    feat(rust): Generate random number and Match result

--- rust/sample/guessing_game/src/main.rs
+++ rust/sample/guessing_game/src/main.rs
@@ -1,14 +1,30 @@
 use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
 
 fn main() {
     println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1..10);
+
+    println!("The secret number is: {}", secret_number);
+
     println!("Type number > ");
 
-    let mut guess = String::new();
+    let mut guess_string = String::new();
 
     io::stdin()
-        .read_line(&mut guess)
+        .read_line(&mut guess_string)
         .expect("Failed to read line");
 
+    let guess: u32 = guess_string.trim().parse()
+        .expect("Type a number > ");
+
     println!("guessed: {}", guess);
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Small < {}", secret_number),
+        Ordering::Greater => println!("Big > {}", secret_number),
+        Ordering::Equal => println!("Equal == {}", secret_number)
+    }
 }
