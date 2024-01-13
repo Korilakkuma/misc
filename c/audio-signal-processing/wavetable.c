07a17a440c825736c08dc0486cc6d844c4e9ddef
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2024-01-13 16:28:55 +0900

    feat(audio-signal-processing): Sine Wave Table by Sweep Function

--- c/audio-signal-processing/wavetable.c
+++ c/audio-signal-processing/wavetable.c
@@ -4,7 +4,10 @@
 
 static float wave_table[2048];
 
-static float last_out = 0.0f;
+static float last_out    = 0.0f;
+static float last_cons_1 = 0.0f;
+static float last_cons_2 = 0.0f;
+
 static float diff = 0.0f;;
 
 static bool interpolated = false;
@@ -47,6 +50,51 @@ int sine(short *data, int start_index, int number_of_samples, int sample_rate, f
 
     ++index;
   }
+
+  return index;
+}
+
+int sine_sweep(short *data, int start_index, int number_of_samples, int sample_rate, float amplitude, float frequency, bool initializing) {
+  float cons_1 = (2.0f * M_PI) / (float)number_of_samples;
+  float cons_2 = frequency / (float)sample_rate;
+
+  if (initializing) {
+    for (int n = 0; n < number_of_samples; n++) {
+      wave_table[n] = sinf(cons_1 * (float)n);
+    }
+
+    return start_index;
+  }
+
+  int index = start_index;
+
+  for (int n = 0; n < number_of_samples; n++) {
+    float w = cons_2 * fmod((float)index, cons_1);
+    float p = (float)number_of_samples * w;
+    float t = ((1.0f - w) * wave_table[(int)p] + (w * wave_table[((int)p == (number_of_samples - 1)) ? 0 : ((int)p + 1)]));
+
+    if (interpolated) {
+      data[n] = (short)t;
+    } else {
+      float w1 = last_cons_2 * fmod((float)index, last_cons_1);
+      float p1 = (float)number_of_samples + w1;
+      float t = ((1.0f - w1) * wave_table[(int)p1] + (w1 * wave_table[(int)p1 == (number_of_samples - 1) ? 0 : ((int)p1 + 1)]));
+
+      data[n] = (short)(((1.0f - diff) * t) + (diff * t));
+
+      if (diff >= 1.0f) {
+        interpolated = true;
+      }
+
+      diff += 0.0008f;
+    }
+
+    ++index;
+  }
+
+  last_out    = frequency;
+  last_cons_1 = cons_1;
+  last_cons_2 = cons_2;
 
   return index;
 }
