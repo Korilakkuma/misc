7383cf65b74021d9141c6bfdf36b60e9018b0ca7
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2024-04-11 21:25:27 +0900

    feat(algorithms): Hash Open Addressing (C++)

--- c++/algorithms/search/hash_open_addressing.cpp
+++ c++/algorithms/search/hash_open_addressing.cpp
@@ -1,5 +1,10 @@
 #include "data.hpp"
 
+enum HashStatus {
+  EMPTY,
+  DELETED
+};
+
 const INT BUCKET_SIZE = 100;
 
 class Cell {
@@ -18,11 +23,17 @@ class Cell {
     INT get_data() {
       return data;
     }
+
+    void set_key(INT newKey) {
+      key = newKey;
+    }
 };
 
 static INT hash(INT key);
+static INT rehash(INT key);
 static bool equal_keys(INT a, INT b);
 static void init_table(std::vector<Cell *> &hash_table);
+
 static Cell *find(INT key);
 static bool insert_into(INT key, INT data);
 static bool delete_from(INT key);
@@ -44,6 +55,10 @@ static INT hash(INT key) {
   return key % BUCKET_SIZE;
 }
 
+static INT rehash(INT hashed_key) {
+  return (hashed_key + 1) % BUCKET_SIZE;
+}
+
 static bool equal_keys(INT a, INT b) {
   return a == b;
 }
@@ -55,13 +70,68 @@ static void init_table(std::vector<Cell *> &hash_table) {
 }
 
 static Cell *find(INT key) {
+  INT h = hash(key);
+  INT rehash_count = 0;
+
+  INT k;
+
+  while ((k = hash_table[h]->get_key()) != EMPTY) {
+    if ((k != DELETED) && equal_keys(key, k)) {
+      return hash_table[h];
+    }
+
+    if (++rehash_count > BUCKET_SIZE) {
+      return nullptr;
+    }
+
+    h = rehash(h);
+  }
+
   return nullptr;
 }
 
 static bool insert_into(INT key, INT data) {
+  INT h = hash(key);
+  INT rehash_count = 1;
+
+  INT k;
+
+  while (((k = hash_table[h]->get_key()) != EMPTY) && (k != DELETED)) {
+    if (equal_keys(key, k)) {
+      return false;
+    }
+
+    if (rehash_count++ > BUCKET_SIZE) {
+      std::cerr << "hash table is overflow" << std::endl;
+      exit(EXIT_FAILURE);
+    }
+
+    h = rehash(h);
+  }
+
+  hash_table[h] = new Cell(key, data);
+
   return true;
 }
 
 static bool delete_from(INT key) {
-  return true;
+  INT h = hash(key);
+  INT rehash_count = 1;
+
+  INT k;
+
+  while ((k = hash_table[h]->get_key()) != EMPTY) {
+    if ((k != DELETED) && equal_keys(key, k)) {
+      hash_table[h]->set_key(DELETED);
+      return true;
+    }
+
+    if (rehash_count++ > BUCKET_SIZE) {
+      return false;
+    }
+
+    h = rehash(h);
+  }
+
+  return false;
 }
