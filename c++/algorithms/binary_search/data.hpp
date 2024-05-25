3692dedd4b8f0af8114634112fc50d3f078a1ae1
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2024-05-25 20:00:02 +0900

    feat(algorithms): Data generator for algorithm on based Binary Search (C++ header file)

---
+++ c++/algorithms/binary_search/data.hpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cmath>
+#include <cstdlib>
+#include <vector>
+
+using INT = long long;
+
+INT generate_random_value(INT max) {
+  return (std::rand() * max) / (RAND_MAX + 1.0);
+}
+
+void init_list(std::vector<INT> &list) {
+  srand((unsigned int)time(NULL));
+
+  for (auto& l : list) {
+    l = generate_random_value(10);
+  }
+}
+
+void dump_list(std::vector<INT> &list) {
+  for (auto& l : list) {
+    std::cout << l << " ";
+  }
+
+  std::cout << std::endl;
+}
