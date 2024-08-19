6abbc4acdc58c0dc3cd1fc1cbb146e7a9a1d2de2
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2024-08-19 23:45:21 +0900

    feat(verilog): Bus

---
+++ verilog/m_bus.v
@@ -0,0 +1,10 @@
+module m_bus();
+  reg [3:0] r_a, r_b;
+  wire [3:0] w_c;
+  assign w_c = r_a | r_b;
+  initial begin
+    r_a <= 4'b1010;
+    r_b <= 4'b1100;
+  end
+  always@(*) #1 $display("%3d %b %b %b", $time, r_a, r_b, w_c);
+endmodule
