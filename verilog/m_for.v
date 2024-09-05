8422e70b8d39565963ab3ae2e34775ab51ad7bb1
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2024-09-05 23:12:06 +0900

    feat(verilog): `for` statement

---
+++ verilog/m_for.v
@@ -0,0 +1,8 @@
+module m_for();
+  reg [31:0] mem [0:1023];
+  integer i;
+  initial for (i = 0; i < 1024; i = i + 1) mem[i] = 0;
+  initial mem[0] <= 7;
+  wire [31:0] w_a = mem[0] + mem[1] + mem[2];
+  initial #1 $display("%d %d %d %d", mem[0], mem[1], mem[2], w_a);
+endmodule
