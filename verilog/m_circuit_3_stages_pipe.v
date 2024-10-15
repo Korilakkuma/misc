08e58103399e2f9f6a7fa91b915c8c7297266502
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2024-10-15 21:39:18 +0900

    feat(verilog): 3-stages pipelining for executing `addi` instruction by using bypass

--- verilog/m_circuit_3_stages_pipe.v
+++ verilog/m_circuit_3_stages_pipe.v
@@ -97,13 +97,18 @@ endmodule
 module m_RF(w_clock, w_ra1, w_ra2, w_rd1, w_rd2, w_wa, w_we, w_wd);
   input wire w_clock, w_we;
   input wire [4:0] w_ra1, w_ra2, w_wa;
-  input wire [31:0] w_wd;
+
   output wire [31:0] w_rd1, w_rd2;
+
+  input wire [31:0] w_wd;
 
   reg [31:0] mem [0:31];
 
-  assign w_rd1 = (w_ra1 == 5'd0) ? 32'd0 : mem[w_ra1];
-  assign w_rd2 = (w_ra2 == 5'd0) ? 32'd0 : mem[w_ra2];
+  wire w_bp1 = (w_we & (w_ra1 == w_wa));
+  wire w_bp2 = (w_we & (w_ra2 == w_wa));
+
+  assign w_rd1 = (w_ra1 == 5'd0) ? 32'd0 : (w_bp1 ? w_wd : mem[w_ra1]);
+  assign w_rd2 = (w_ra2 == 5'd0) ? 32'd0 : (w_bp2 ? w_wd : mem[w_ra2]);
 
   always @(posedge w_clock) if (w_we) mem[w_wa] <= w_wd;
   always @(posedge w_clock) if (w_we & (w_wa == 5'd30)) $finish;
