cda184851ccf28cf18158fc0712c0f149d3a9de7
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2024-10-16 20:50:37 +0900

    feat(verilog); 3-stages pipelining for executing `addi` and `bne` instructions by using bypass and flush

--- verilog/m_circuit_3_stages_pipe.v
+++ verilog/m_circuit_3_stages_pipe.v
@@ -21,7 +21,7 @@ module m_circuit_3_stages_pipe(w_clock);
 
   reg [31:0] P2_r1 = 0, P2_s2 = 0, P2_r2 = 0, P2_tpc = 0;
 
-  reg P2_s = 0, P2_b = 0, P2_ld = 0;
+  reg P2_s = 0, P2_b = 0, P2_ld = 0, P1_v = 0, P2_v = 0;
 
   reg [4:0] P2_rd = 0;
 
@@ -32,16 +32,19 @@ module m_circuit_3_stages_pipe(w_clock);
 
   reg[31:0] r_pc = 0;
 
-  m_mux multiplexer_if (w_npc, P2_tpc, (P2_b & w_token), w_pcin);
+  wire w_miss = P2_b & w_token & P2_v;
+
+  m_mux multiplexer_if (w_npc, P2_tpc, w_miss, w_pcin);
   m_adder adder_if (32'h4, r_pc, w_npc);
   m_async_imem imem_if (r_pc, w_ir);
 
   m_gen_imm imm_id (P1_ir, w_imm, w_r, w_i, w_s, w_b, w_u, w_j, w_ld);
-  m_RF rf_id (w_clock, P1_ir[19:15], P1_ir[24:20], w_r1, w_r2, P2_rd, (!P2_s & !P2_b), w_rt);
+  m_RF rf_id (w_clock, P1_ir[19:15], P1_ir[24:20], w_r1, w_r2, P2_rd, (!P2_s & !P2_b & P2_v), w_rt);
   m_adder adder_id (w_imm, P1_pc, w_tpc);
   m_mux multiplexer_id (w_r2, w_imm, (!w_r & !w_b), w_s2);
 
   always @(posedge w_clock) begin
+    { P1_v, P2_v } <= { !w_miss, !w_miss & P1_v };
     { r_pc, P1_ir, P1_pc } <= { w_pcin, w_ir, r_pc };
     { P2_r1, P2_r2, P2_s2 } <= { w_r1, w_r2, w_s2 };
     { P2_pc, P2_tpc, P2_rd } <= { P1_pc, w_tpc, P1_ir[11:7] };
@@ -50,7 +53,7 @@ module m_circuit_3_stages_pipe(w_clock);
 
   m_alu alu_ex (P2_r1, P2_s2, w_alu, w_token);
 
-  m_async_data_mem mem_ma (w_clock, w_alu, P2_s, P2_r2, w_ldd);
+  m_async_data_mem mem_ma (w_clock, w_alu, (P2_s & P2_v), P2_r2, w_ldd);
 
   m_mux multiplexer_wb (w_alu, w_ldd, P2_ld, w_rt);
 
