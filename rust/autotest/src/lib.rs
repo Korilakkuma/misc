0062ea0d11cca2d8e320922e6d25865b46f49b91
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2024-11-16 22:00:00 +0900

    feat(rust): Automatic test (Use `assert!` macro)

--- rust/autotest/src/lib.rs
+++ rust/autotest/src/lib.rs
@@ -1,7 +1,36 @@
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        (self.width > other.width) && (self.height > other.height)
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
     fn it_works() {
         assert_eq!((2 + 2), 4);
+    }
+
+    #[test]
+    fn it_can_hold() {
+        let larger = Rectangle {
+            width: 8,
+            height: 7,
+        };
+
+        let smaller = Rectangle {
+            width: 5,
+            height: 1,
+        };
+
+        assert!(larger.can_hold(&smaller));
     }
 }
