aed4d7dc46b8e35f7217a38e9377795ce753244a
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2024-11-22 23:12:48 +0900

    feat(rust): Error handling (CLI program)

--- rust/minigrep/src/main.rs
+++ rust/minigrep/src/main.rs
@@ -1,22 +1,24 @@
 use std::env;
+use std::process;
+use std::error::Error;
 use std::fs::File;
 use std::io::prelude::*;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::new(&args);
+    let config = Config::new(&args).unwrap_or_else(|error| {
+        println!("{}", error);
+        process::exit(1);
+    });
 
     println!("Searching for {}", config.query);
     println!("in file {}", config.filename);
 
-    let mut file = File::open(config.filename).expect("File is not found");
-
-    let mut contents = String::new();
-
-    file.read_to_string(&mut contents).expect("Error occurred in reading file");
-
-    println!("File contents:\n{}", contents);
+    if let Err(error) = run(config) {
+        println!("Application error: {}", error);
+        process::exit(1);
+    }
 }
 
 struct Config {
@@ -25,10 +27,26 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("cargo run 'query' 'filename'");
+        }
+
         let query = args[1].clone();
         let filename = args[2].clone();
 
-        Config { query, filename }
+        Ok(Config { query, filename })
     }
+}
+
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let mut file = File::open(config.filename)?;
+
+    let mut contents = String::new();
+
+    file.read_to_string(&mut contents)?;
+
+    println!("File contents:\n{}", contents);
+
+    Ok(())
 }
