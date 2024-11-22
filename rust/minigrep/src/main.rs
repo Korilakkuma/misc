0ad0b07d0336032c205f8580347bdb05cd418049
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2024-11-22 22:56:55 +0900

    feat(rust): Implement struct has parsed command line arguments (CLI program)

--- rust/minigrep/src/main.rs
+++ rust/minigrep/src/main.rs
@@ -5,17 +5,30 @@ use std::io::prelude::*;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let filename = &args[2];
+    let config = Config::new(&args);
 
-    println!("Searching for {}", query);
-    println!("in file {}", filename);
+    println!("Searching for {}", config.query);
+    println!("in file {}", config.filename);
 
-    let mut file = File::open(filename).expect("File is not found");
+    let mut file = File::open(config.filename).expect("File is not found");
 
     let mut contents = String::new();
 
     file.read_to_string(&mut contents).expect("Error occurred in reading file");
 
     println!("File contents:\n{}", contents);
+}
+
+struct Config {
+    query: String,
+    filename: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Config {
+        let query = args[1].clone();
+        let filename = args[2].clone();
+
+        Config { query, filename }
+    }
 }
