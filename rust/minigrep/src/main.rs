c26d19fbf17dd642871d0b62113311647f0c764e
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2024-11-23 18:00:14 +0900

    feat(rust): Define structure and function as library crate (CLI program)

--- rust/minigrep/src/main.rs
+++ rust/minigrep/src/main.rs
@@ -1,8 +1,9 @@
+extern crate minigrep;
+
 use std::env;
 use std::process;
-use std::error::Error;
-use std::fs::File;
-use std::io::prelude::*;
+
+use minigrep::{ Config, run };
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -19,34 +20,4 @@ fn main() {
         println!("Application error: {}", error);
         process::exit(1);
     }
-}
-
-struct Config {
-    query: String,
-    filename: String,
-}
-
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("cargo run 'query' 'filename'");
-        }
-
-        let query = args[1].clone();
-        let filename = args[2].clone();
-
-        Ok(Config { query, filename })
-    }
-}
-
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let mut file = File::open(config.filename)?;
-
-    let mut contents = String::new();
-
-    file.read_to_string(&mut contents)?;
-
-    println!("File contents:\n{}", contents);
-
-    Ok(())
 }
