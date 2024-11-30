d7bda6e94b8ce25e00d14476aeeaf49ed2f2a13b
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2024-11-30 22:00:00 +0900

    feat(rust): Ring Buffer for Voice Changer

--- rust/audio/voice-changer/wasm-audio/src/lib.rs
+++ rust/audio/voice-changer/wasm-audio/src/lib.rs
@@ -10,3 +10,145 @@ use rustfft::{
 use wasm_bindgen::prelude::*;
 
 extern crate web_sys;
+
+struct RingBuffer<T: Clone + Copy> {
+    buffer: Vec<T>,
+    capacity: usize,
+    front: usize,
+    rear: usize,
+}
+
+impl<T: Clone + Copy> RingBuffer<T> {
+    pub fn new(capacity: usize, value: T) -> RingBuffer<T> {
+        RingBuffer {
+            buffer: vec![value; capacity + 1],
+            capacity,
+            front: 0,
+            rear: 0,
+        }
+    }
+
+    pub fn capacity(&self) -> usize {
+        self.capacity
+    }
+
+    pub fn readable_elements(&self) -> usize {
+        if self.front <= self.rear {
+            self.rear - self.front
+        } else {
+            self.rear + (self.capacity + 1) - self.front
+        }
+    }
+
+    pub fn writable_elements(&self) -> usize {
+        self.capacity - self.readable_elements()
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.readable_elements() == 0
+    }
+
+    pub fn is_full(&self) -> bool {
+        self.writable_elements() == 0
+    }
+
+    pub fn read(&mut self, dest: &mut [T]) -> bool {
+        let length = dest.len();
+
+        if self.readable_elements() < length {
+            return false;
+        }
+
+        let buffer_length = self.capacity + 1;
+
+        let copy_min = std::cmp::min((buffer_length - self.front), length);
+
+        dest[0..copy_min].clone_from_slice(&self.buffer[self.front..(self.front + copy_min)]);
+
+        if copy_min == length {
+            return true;
+        }
+
+        let copy_max = std::cmp::max(length, copy_min) - copy_min;
+
+        dest[copy_min..(copy_min + copy_max)].clone_from_slice(&self.buffer[0..copy_max]);
+
+        true
+    }
+
+    pub fn write(&mut self, src: &[T]) -> bool {
+        let length = src.len();
+
+        if self.writable_elements() < length {
+            return false;
+        }
+
+        let buffer_length = self.capacity + 1;
+
+        let copy_min = std::cmp::min((buffer_length - self.rear), length);
+
+        self.buffer[self.rear..(self.rear + copy_min)].clone_from_slice(&src[0..copy_min]);
+
+        if copy_min == length {
+            self.rear += copy_min;
+            return true;
+        }
+
+        let copy_diff = length - copy_min;
+
+        self.buffer[0..copy_diff].clone_from_slice(&src[copy_min..(copy_min + copy_diff)]);
+
+        self.rear = copy_diff;
+
+        true
+    }
+
+    pub fn fill(&mut self, length: usize, value: T) -> bool {
+        if self.writable_elements() < length {
+            return false;
+        }
+
+        let buffer_length = self.capacity + 1;
+
+        let copy_min = std::cmp::min((buffer_length - self.rear), length);
+
+        self.buffer[self.rear..(self.rear + copy_min)].fill(value.clone());
+
+        if copy_min == length {
+            self.rear += copy_min;
+            return true;
+        }
+
+        let copy_diff = buffer_length - copy_min;
+
+        self.buffer[0..copy_diff].fill(value.clone());
+        self.rear = copy_diff;
+
+        true
+    }
+
+    pub fn discard(&mut self, length: usize) -> bool {
+        if self.readable_elements() < length {
+            return false;
+        }
+
+        let buffer_length = self.capacity + 1;
+
+        let discard_min = std::cmp::min((buffer_length - self.front), length);
+
+        if discard_min == length {
+            self.front += discard_min;
+            return true;
+        }
+
+        let discard_diff = length - discard_min;
+
+        self.front = discard_diff;
+
+        true
+    }
+
+    pub fn discard_all(&mut self) -> bool {
+        self.discard(self.readable_elements())
+    }
+}
