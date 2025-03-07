dbfd66e16902b747a13ff4868c85ff48662b105c
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2025-03-07 23:12:09 +0900

    feat(audio): Adaptive Filter (uses only step size)

--- javascript/signaling/app/adaptive-filter.js
+++ javascript/signaling/app/adaptive-filter.js
@@ -63,3 +63,51 @@ const cancelEcho = (stream) => {
    }
   };
 };
+
+const cancelEcho2 = (stream) => {
+  const context = new AudioContext();
+
+  const source = new MediaStreamAudioSourceNode(context, stream);
+
+  const processor = context.createScriptProcessor(8192, 1, 1);
+
+  source.connect(processor);
+  processor.connect(context.destination);
+
+  const bufferSize = processor.bufferSize;
+
+  const filterLength = 1024;
+
+  const step = 0.01;
+
+  processor.onaudioprocess = (event) => {
+    const inputs  = event.inputBuffer.getChannelData(0);
+    const outputs = event.outputBuffer.getChannelData(0);
+
+    const referenceSignal = new Float32Array(bufferSize);
+
+    const adaptiveFilter = new Float32Array(filterLength);
+
+    const referenceSignalBuffer = new Float32Array(filterLength);
+
+    for (let t = 0; t < bufferSize; t++) {
+      for (let n = filterLength - 1; n > 0; n--) {
+        referenceSignalBuffer[n] = referenceSignalBuffer[n - 1];
+      }
+
+      referenceSignalBuffer[0] = referenceSignal[t]
+
+      let err = 0;
+
+      for (let n = 0; n < filterLength; n++) {
+        err += adaptiveFilter[n] * referenceSignalBuffer[n];
+      }
+
+      outputs[t] = input[t] - err;
+
+      for (let n = 0; n < filterLength; n++) {
+        adaptiveFilter[n] += (step * outputs[t]) * referenceSignalBuffer[n];
+      }
+    }
+  };
+};
