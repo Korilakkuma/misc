8617d2615d05af979e4c02d341e100f4dbd13736
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2025-05-31 20:25:44 +0900

    feat(concurrency): Scheduling by Coroutine and async/await

---
+++ rust/concurrency/async_await/src/main.rs
@@ -0,0 +1,127 @@
+use futures::future::{BoxFuture, FutureExt};
+use futures::task::{ArcWake, waker_ref};
+use std::future::Future;
+use std::pin::Pin;
+use std::sync::mpsc::{Receiver, SyncSender, sync_channel};
+use std::sync::{Arc, Mutex};
+use std::task::{Context, Poll};
+
+enum MessageState {
+    START,
+    SEND,
+    END,
+}
+
+struct Message {
+    state: MessageState,
+}
+
+impl Message {
+    fn new() -> Self {
+        Message {
+            state: MessageState::START,
+        }
+    }
+}
+
+impl Future for Message {
+    type Output = ();
+
+    fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<()> {
+        match (*self).state {
+            MessageState::START => {
+                println!("Start message ...");
+
+                (*self).state = MessageState::SEND;
+
+                cx.waker().wake_by_ref();
+
+                Poll::Pending
+            }
+            MessageState::SEND => {
+                println!("Send 'Message !'");
+
+                (*self).state = MessageState::END;
+
+                cx.waker().wake_by_ref();
+
+                Poll::Pending
+            }
+            MessageState::END => Poll::Ready(()),
+        }
+    }
+}
+
+struct Task {
+    future: Mutex<BoxFuture<'static, ()>>,
+    sender: SyncSender<Arc<Task>>,
+}
+
+impl ArcWake for Task {
+    fn wake_by_ref(arc_self: &Arc<Self>) {
+        let self_0 = arc_self.clone();
+
+        arc_self.sender.send(self_0).unwrap();
+    }
+}
+
+struct Executor {
+    sender: SyncSender<Arc<Task>>,
+    receiver: Receiver<Arc<Task>>,
+}
+
+struct Spawner {
+    sender: SyncSender<Arc<Task>>,
+}
+
+impl Spawner {
+    fn spawn(&self, future: impl Future<Output = ()> + 'static + Send) {
+        let future = future.boxed();
+
+        let task = Arc::new(Task {
+            future: Mutex::new(future),
+            sender: self.sender.clone(),
+        });
+
+        self.sender.send(task).unwrap();
+    }
+}
+
+impl Executor {
+    fn new() -> Self {
+        let (sender, receiver) = sync_channel(1024);
+
+        Executor {
+            sender: sender.clone(),
+            receiver,
+        }
+    }
+
+    fn get_spawner(&self) -> Spawner {
+        Spawner {
+            sender: self.sender.clone(),
+        }
+    }
+
+    fn run(&self) {
+        while let Ok(task) = self.receiver.recv() {
+            let mut future = task.future.lock().unwrap();
+            let waker = waker_ref(&task);
+            let mut ctx = Context::from_waker(&waker);
+
+            let _ = future.as_mut().poll(&mut ctx);
+        }
+    }
+}
+
+fn main() {
+    let executor = Executor::new();
+
+    executor.get_spawner().spawn(async {
+        let m = Message::new();
+
+        m.await;
+    });
+
+    executor.run();
+}
