d75c27be1bb602e141dee356407abe488846a118
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2025-07-03 20:45:49 +0900

    feat(concurrency): Cooperative Green threads (Cooperative Multitasking or Non-Preemptive Multitasking)
    `main` function

--- rust/concurrency/cooperative_green_threads/src/main.rs
+++ rust/concurrency/cooperative_green_threads/src/main.rs
@@ -1,1 +1,30 @@
-fn main() {}
+mod green;
+
+fn main() {
+    green::spawn_from_main(gaia, 2 * 1024 * 1024); // 2 MiB
+}
+
+fn ortega() {
+    for _ in 0..10 {
+        println!("Call ortega");
+        green::schedule();
+    }
+}
+
+fn mash() {
+    green::spawn(ortega, 2 * 1024 * 1024);
+
+    for _ in 0..10 {
+        println!("Call mash");
+        green::schedule();
+    }
+}
+
+fn gaia() {
+    green::spawn(mash, 2 * 1024 * 1024);
+
+    for _ in 0..10 {
+        println!("Call gaia");
+        green::schedule();
+    }
+}
