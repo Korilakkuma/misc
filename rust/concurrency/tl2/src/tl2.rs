907ac9e429db11631994a27ffeb08e561b5a5cd6
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2025-07-14 22:00:00 +0900

    feat(concurrency): Software Transactional Memory (TL2)
    WriteTrans

--- rust/concurrency/tl2/src/tl2.rs
+++ rust/concurrency/tl2/src/tl2.rs
@@ -1,5 +1,6 @@
 use std::cell::UnsafeCell;
 use std::collections::{HashMap, HashSet};
+use std::ops::Drop;
 use std::sync::atomic::{fence, AtomicU64, Ordering};
 
 const SIZE_OF_STRIPE: usize = 8; // u64, 8 Bytes (It is require to be 2^{n}
@@ -121,5 +122,133 @@ impl<'a> ReadTrans<'a> {
         }
 
         Some(mem)
+    }
+}
+
+pub struct WriteTrans<'a> {
+    read_ver: u64,
+    read_set: HashSet<usize>,
+    write_set: HashMap<usize, [u8; SIZE_OF_STRIPE]>,
+    locked: Vec<usize>,
+    is_abort: bool,
+    mem: &'a mut Memory,
+}
+
+impl<'a> WriteTrans<'a> {
+    fn new(mem: &'a mut Memory) -> Self {
+        WriteTrans {
+            read_set: HashSet::new(),
+            write_set: HashMap::new(),
+            locked: Vec::new(),
+            is_abort: false,
+            read_ver: mem.global_clock.load(Ordering::Acquire),
+            mem,
+        }
+    }
+
+    pub fn store(&mut self, addr: usize, val: [u8; SIZE_OF_STRIPE]) {
+        assert_eq!(addr & (SIZE_OF_STRIPE - 1), 0);
+
+        self.write_set.insert(addr, val);
+    }
+
+    pub fn load(&mut self, addr: usize) -> Option<[u8; SIZE_OF_STRIPE]> {
+        if self.is_abort {
+            return None;
+        }
+
+        assert_eq!(addr & (SIZE_OF_STRIPE - 1), 0);
+
+        self.read_set.insert(addr);
+
+        if let Some(m) = self.write_set.get(&addr) {
+            return Some(*m);
+        }
+
+        if !self.mem.test_not_modify(addr, self.read_ver) {
+            self.is_abort = true;
+            return None;
+        }
+
+        fence(Ordering::Acquire);
+
+        let mut mem = [0; SIZE_OF_STRIPE];
+
+        for (dest, src) in mem
+            .iter_mut()
+            .zip(self.mem.mem[addr..addr + SIZE_OF_STRIPE].iter())
+        {
+            *dest = *src;
+        }
+
+        fence(Ordering::SeqCst);
+
+        if !self.mem.test_not_modify(addr, self.read_ver) {
+            self.is_abort = true;
+            return None;
+        }
+
+        Some(mem)
+    }
+
+    fn lock_write_set(&mut self) -> bool {
+        for (addr, _) in self.write_set.iter() {
+            if self.mem.lock_addr(*addr) {
+                self.locked.push(*addr);
+            } else {
+                return false;
+            }
+        }
+
+        true
+    }
+
+    fn validate_read_set(&self) -> bool {
+        for addr in self.read_set.iter() {
+            if self.write_set.contains_key(addr) {
+                let ver = self.mem.get_addr_ver(*addr);
+
+                if ver > self.read_ver {
+                    return false;
+                }
+            } else {
+                if !self.mem.test_not_modify(*addr, self.read_ver) {
+                    return false;
+                }
+            }
+        }
+
+        true
+    }
+
+    fn commit(&mut self, ver: u64) {
+        for (addr, val) in self.write_set.iter() {
+            let addr = *addr as usize;
+
+            for (dest, src) in self.mem.mem[addr..addr + SIZE_OF_STRIPE]
+                .iter_mut()
+                .zip(val)
+            {
+                *dest = *src
+            }
+        }
+
+        fence(Ordering::Release);
+
+        for (addr, _) in self.write_set.iter() {
+            let index = addr >> self.mem.shift_size;
+
+            self.mem.lock_ver[index].store(ver, Ordering::Relaxed);
+        }
+
+        self.locked.clear();
+    }
+}
+
+impl<'a> Drop for WriteTrans<'a> {
+    fn drop(&mut self) {
+        for addr in self.locked.iter() {
+            self.mem.unlock_addr(*addr);
+        }
     }
 }
