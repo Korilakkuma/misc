3a5e8421b07ac365ed959f2920c453456d813d42
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2025-07-21 20:00:00 +0900

    feat(concurrency): Software Transactional Memory (TL2)
    `macro_rules!` for load and store

--- rust/concurrency/tl2/src/main.rs
+++ rust/concurrency/tl2/src/main.rs
@@ -1,3 +1,21 @@
 mod tl2;
 
+#[macro_export]
+macro_rules! load {
+    ($t:ident, $a:expr) => {
+        if let Some(v) = ($t).load($a) {
+            v
+        } else {
+            return tl2::STMResult::Retry;
+        }
+    };
+}
+
+#[macro_export]
+macro_rules! store {
+    ($t:ident, $a:expr, $v:expr) => {
+        $t.store($a, $v)
+    };
+}
+
 fn main() {}
