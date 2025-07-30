9878409873d5f2bd72c88a1021b64c5d5e1bf9cd
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2025-07-30 22:00:00 +0900

    feat(concurrency): Session
    `main` function

--- rust/concurrency/session/src/main.rs
+++ rust/concurrency/session/src/main.rs
@@ -1,1 +1,24 @@
-fn main() {}
+mod channel;
+mod client;
+mod server;
+
+use crate::channel::{Op, S};
+use crate::client::client;
+use crate::server::server;
+use std::thread;
+
+fn main() {
+    let (server_chan, client_chan) = S::session_channel();
+    let srv_t = thread::spawn(move || server(server_chan));
+    let cli_t = thread::spawn(move || client(client_chan, 11, Op::Even));
+
+    srv_t.join().unwrap();
+    cli_t.join().unwrap();
+
+    let (server_chan, client_chan) = S::session_channel();
+    let srv_t = thread::spawn(move || server(server_chan));
+    let cli_t = thread::spawn(move || client(client_chan, 11, Op::Square));
+
+    srv_t.join().unwrap();
+    cli_t.join().unwrap();
+}
