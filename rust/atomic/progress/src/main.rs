36bd9abc2887dbf9cacb477dd2ed2bcb01659c44
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2025-08-26 18:00:00 +0900

    feat(atomic): Progress by Thread Parking

--- rust/atomic/progress/src/main.rs
+++ rust/atomic/progress/src/main.rs
@@ -4,6 +4,7 @@ use std::thread;
 use std::thread::sleep;
 use std::time::Duration;
 
+/*
 fn main() {
     let number_of_done = AtomicUsize::new(0);
 
@@ -25,6 +26,37 @@ fn main() {
             println!("Progress ... {n} / 100 done");
 
             sleep(Duration::from_secs(1));
+        }
+    });
+
+    println!("Done !");
+}
+*/
+
+fn main() {
+    let number_of_done = AtomicUsize::new(0);
+
+    let main_thread = thread::current();
+
+    thread::scope(|s| {
+        s.spawn(|| {
+            for n in 0..100 {
+                sleep(Duration::from_secs(1));
+                number_of_done.store(n + 1, Ordering::Relaxed);
+                main_thread.unpark();
+            }
+        });
+
+        loop {
+            let n = number_of_done.load(Ordering::Relaxed);
+
+            if n == 100 {
+                break;
+            }
+
+            println!("Progress ... {n} / 100 done");
+
+            thread::park_timeout(Duration::from_secs(1));
         }
     });
 
