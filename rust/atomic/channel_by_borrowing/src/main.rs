1bfd8487b690457488358d2981cf86312222bd68
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2025-10-07 20:00:00 +0900

    feat(atomic): Channel by Borrowing (Add Blocking)

--- rust/atomic/channel_by_borrowing/src/main.rs
+++ rust/atomic/channel_by_borrowing/src/main.rs
@@ -1,9 +1,10 @@
 use std::cell::UnsafeCell;
-use std::marker::{Send, Sync};
+use std::marker::{PhantomData, Send, Sync};
 use std::mem::MaybeUninit;
 use std::ops::Drop;
 use std::sync::atomic::{AtomicBool, Ordering};
 use std::thread;
+use std::thread::Thread;
 
 pub struct Channel<T> {
     message: UnsafeCell<MaybeUninit<T>>,
@@ -14,10 +15,12 @@ unsafe impl<T: Send + Sync> Sync for Channel<T> {}
 
 pub struct Sender<'a, T> {
     channel: &'a Channel<T>,
+    receiving_thread: Thread,
 }
 
 pub struct Receiver<'a, T> {
     channel: &'a Channel<T>,
+    _no_send: PhantomData<*const ()>,
 }
 
 impl<T> Drop for Channel<T> {
@@ -41,7 +44,16 @@ impl<T> Channel<T> {
     pub fn split<'a>(&'a mut self) -> (Sender<'a, T>, Receiver<'a, T>) {
         *self = Self::new();
 
-        (Sender { channel: self }, Receiver { channel: self })
+        (
+            Sender {
+                channel: self,
+                receiving_thread: thread::current(),
+            },
+            Receiver {
+                channel: self,
+                _no_send: PhantomData,
+            },
+        )
     }
 }
 
@@ -50,6 +62,7 @@ impl<T> Sender<'_, T> {
         unsafe { (*self.channel.message.get()).write(message) };
 
         self.channel.ready.store(true, Ordering::Release);
+        self.receiving_thread.unpark();
     }
 }
 
@@ -59,8 +72,8 @@ impl<T> Receiver<'_, T> {
     }
 
     pub fn receive(self) -> T {
-        if !self.channel.ready.swap(false, Ordering::Acquire) {
-            panic!("No message available !");
+        while !self.channel.ready.swap(false, Ordering::Acquire) {
+            thread::park();
         }
 
         unsafe { (*self.channel.message.get()).assume_init_read() }
@@ -73,16 +86,9 @@ fn main() {
     thread::scope(|s| {
         let (sender, receiver) = channel.split();
 
-        let t = thread::current();
-
         s.spawn(move || {
             sender.send("Hello World !");
-            t.unpark();
         });
-
-        while !receiver.is_ready() {
-            thread::park();
-        }
 
         assert_eq!(receiver.receive(), "Hello World !");
     });
