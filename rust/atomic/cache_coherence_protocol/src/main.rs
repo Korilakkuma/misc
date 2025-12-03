eedfc9e205f1fdc30fff74e67c9bfe68958bbbb1
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2025-12-03 21:27:21 +0900

    feat(atomic): Cache Coherence Protocol

---
+++ rust/atomic/cache_coherence_protocol/src/main.rs
@@ -0,0 +1,17 @@
+use std::hint::black_box;
+use std::sync::atomic::{AtomicU64, Ordering};
+use std::time::Instant;
+
+static A: AtomicU64 = AtomicU64::new(0);
+
+fn main() {
+    black_box(&A);
+
+    let start = Instant::now();
+
+    for _ in 0..1_000_000_000 {
+        black_box(A.load(Ordering::Relaxed));
+    }
+
+    println!("{:?}", start.elapsed());
+}
