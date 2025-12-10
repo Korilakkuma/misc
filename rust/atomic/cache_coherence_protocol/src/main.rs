3a9827806127d988b88535bd0cd94b777d3382f1
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2025-12-10 21:43:54 +0900

    feat(atomic): Cache Coherence Protocol (with background thread)

--- rust/atomic/cache_coherence_protocol/src/main.rs
+++ rust/atomic/cache_coherence_protocol/src/main.rs
@@ -1,9 +1,27 @@
 use std::hint::black_box;
 use std::sync::atomic::{AtomicU64, Ordering};
+use std::thread;
 use std::time::Instant;
 
 static A: AtomicU64 = AtomicU64::new(0);
 
+fn main() {
+    black_box(&A);
+
+    thread::spawn(|| loop {
+        black_box(A.load(Ordering::Relaxed));
+    });
+
+    let start = Instant::now();
+
+    for _ in 0..1_000_000_000 {
+        black_box(A.load(Ordering::Relaxed));
+    }
+
+    println!("{:?}", start.elapsed());
+}
+
+/*
 fn main() {
     black_box(&A);
 
@@ -15,3 +33,4 @@ fn main() {
 
     println!("{:?}", start.elapsed());
 }
+*/
