3201bcf020c5156bfc5a7b76704630a80c0629c6
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2025-12-12 21:22:09 +0900

    feat(atomic): Cache Line

---
+++ rust/atomic/cache_line/src/main.rs
@@ -0,0 +1,23 @@
+use std::hint::black_box;
+use std::sync::atomic::{AtomicU64, Ordering};
+use std::thread;
+use std::time::Instant;
+
+static A: [AtomicU64; 3] = [AtomicU64::new(0), AtomicU64::new(0), AtomicU64::new(0)];
+
+fn main() {
+    black_box(&A);
+
+    thread::spawn(|| loop {
+        A[0].store(0, Ordering::Relaxed);
+        A[2].store(0, Ordering::Relaxed);
+    });
+
+    let start = Instant::now();
+
+    for _ in 0..1_000_000_000 {
+        black_box(A[1].load(Ordering::Relaxed));
+    }
+
+    println!("{:?}", start.elapsed());
+}
