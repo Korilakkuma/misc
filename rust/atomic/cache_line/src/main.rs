314bc8170e8106b1c8da59864e4cf019936d9c1d
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2025-12-13 21:35:05 +0900

    feat(atomic): Cache Line (use struct alignment)

--- rust/atomic/cache_line/src/main.rs
+++ rust/atomic/cache_line/src/main.rs
@@ -3,6 +3,33 @@ use std::sync::atomic::{AtomicU64, Ordering};
 use std::thread;
 use std::time::Instant;
 
+#[repr(align(64))]
+struct Aligned(AtomicU64);
+
+static A: [Aligned; 3] = [
+    Aligned(AtomicU64::new(0)),
+    Aligned(AtomicU64::new(0)),
+    Aligned(AtomicU64::new(0)),
+];
+
+fn main() {
+    black_box(&A);
+
+    thread::spawn(|| loop {
+        A[0].0.store(1, Ordering::Relaxed);
+        A[2].0.store(1, Ordering::Relaxed);
+    });
+
+    let start = Instant::now();
+
+    for _ in 0..1_000_000_000 {
+        black_box(A[1].0.load(Ordering::Relaxed));
+    }
+
+    println!("{:?}", start.elapsed());
+}
+
+/*
 static A: [AtomicU64; 3] = [AtomicU64::new(0), AtomicU64::new(0), AtomicU64::new(0)];
 
 fn main() {
@@ -21,3 +48,4 @@ fn main() {
 
     println!("{:?}", start.elapsed());
 }
+*/
