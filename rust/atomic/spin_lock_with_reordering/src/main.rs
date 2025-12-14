79e4ddc1bbd455dd9651d2d24f6028e86bdf240b
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2025-12-14 21:45:49 +0900

    feat(atomic): Spin Lock with Reordering

---
+++ rust/atomic/spin_lock_with_reordering/src/main.rs
@@ -0,0 +1,34 @@
+use std::sync::atomic::{compiler_fence, AtomicBool, AtomicUsize, Ordering};
+use std::thread;
+
+fn main() {
+    let locked = AtomicBool::new(false);
+    let counter = AtomicUsize::new(0);
+
+    thread::scope(|s| {
+        for _ in 0..4 {
+            s.spawn(|| {
+                for _ in 0..1_000_000 {
+                    while locked.swap(true, Ordering::Relaxed) {}
+                    // while locked.swap(true, Ordering::Release) {}
+
+                    compiler_fence(Ordering::Acquire);
+
+                    let old = counter.load(Ordering::Relaxed);
+                    // let old = counter.load(Ordering::Acquire);
+                    let new = old + 1;
+
+                    counter.store(new, Ordering::Relaxed);
+                    // counter.store(new, Ordering::Release);
+
+                    compiler_fence(Ordering::Release);
+
+                    locked.store(false, Ordering::Relaxed);
+                    // locked.store(false, Ordering::Release);
+                }
+            });
+        }
+    });
+
+    println!("{}", counter.into_inner());
+}
