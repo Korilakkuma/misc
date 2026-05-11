62370b9e7c90b11cb756eee73737eae0c2669a89
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2026-05-11 22:27:23 +0900

    feat(system-call): Write text to stdout (`STDOUT_FILENO`)

---
+++ c/system-call/io/to_stdout.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+int main(int argc, char **argv) {
+  if (argc != 2) {
+    fputs("./to_stdout filename\n", stdout);
+    exit(EXIT_FAILURE);
+  }
+
+  int fd;
+
+  size_t size = 64;
+  char buf[size];
+  ssize_t byte;
+
+  char *filename = argv[1];
+
+  if ((fd = open(filename, O_RDONLY)) == -1) {
+    perror("close");
+    exit(EXIT_FAILURE);
+  }
+
+  while ((byte = read(fd, buf, size)) > 0) {
+    if (write(STDOUT_FILENO, buf, size) != size) {
+      perror("write");
+      exit(EXIT_FAILURE);
+    }
+  }
+
+  if (byte == -1) {
+    perror("write");
+    exit(EXIT_FAILURE);
+  }
+
+  if (close(fd) == -1) {
+    perror("close");
+    exit(EXIT_FAILURE);
+  }
+
+  return 0;
+}
