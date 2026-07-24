fff1251796151e7376ac99e931aeac7d4c4ff4ad
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2026-07-24 21:27:43 +0900

    feat(system-call): Concurrent server

--- c/system-call/socket/server.c
+++ c/system-call/socket/server.c
@@ -1,9 +1,11 @@
 #include <arpa/inet.h>
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/socket.h>
 #include <sys/types.h>
+#include <sys/wait.h>
 #include <unistd.h>
 
 int main(void) {
@@ -48,6 +50,19 @@ int main(void) {
   }
 
   while (1) {
+    pid_t zombie_pid;
+    int status;
+
+    while ((zombie_pid = waitpid(-1, &status, WNOHANG)) > 0) {
+      fprintf(stdout, "process (PID %d) is salvaged.\n", zombie_pid);
+    }
+
+    // Background Jobs exist ?
+    if ((zombie_pid == -1) && (errno != ECHILD)) {
+      perror("waitpid");
+      exit(EXIT_FAILURE);
+    }
+
     socklen_t ca_len = sizeof(ca);
 
     fputs("Waiting for connection ...\n", stdout);
@@ -58,27 +73,43 @@ int main(void) {
 
     fputs("Connection Established !\n", stdout);
 
-    fputs("Type messaeg > ", stdout);
+    pid_t new_server_process = fork();
 
-    while (fgets(buf, sizeof(buf), stdin) != NULL) {
-      if ((written_byte = write(ws, buf, strlen(buf))) == -1) {
-        perror("write");
+    if (new_server_process == -1) {
+      perror("fork");
+      exit(EXIT_FAILURE);
+    }
+
+    if (new_server_process == 0) {
+      close(s);
+
+      fputs("Type messaeg > ", stdout);
+
+      while (fgets(buf, sizeof(buf), stdin) != NULL) {
+        if ((written_byte = write(ws, buf, strlen(buf))) == -1) {
+          perror("write");
+          exit(EXIT_FAILURE);
+        }
+
+        fputs("Type messaeg > ", stdout);
+      }
+
+      clearerr(stdin);
+
+      if (shutdown(ws, SHUT_RDWR) == -1) {
+        perror("shutdown");
         exit(EXIT_FAILURE);
       }
 
-      fputs("Type messaeg > ", stdout);
-    }
-
-    clearerr(stdin);
-
-    if (shutdown(ws, SHUT_RDWR) == -1) {
-      perror("shutdown");
-      exit(EXIT_FAILURE);
-    }
-
-    if (close(ws) == -1) {
-      perror("close");
-      exit(EXIT_FAILURE);
+      if (close(ws) == -1) {
+        perror("close");
+        exit(EXIT_FAILURE);
+      }
+    } else {
+      if (close(ws) == -1) {
+        perror("close");
+        exit(EXIT_FAILURE);
+      }
     }
   }
 
