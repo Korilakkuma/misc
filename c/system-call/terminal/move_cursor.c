1a34552b24d8c86b8f147f84a0722373393d2917
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2026-09-18 19:02:49 +0900

    feat(system-call): Move cursor (by `tparm` library function)

---
+++ c/system-call/terminal/move_cursor.c
@@ -0,0 +1,16 @@
+#include <curses.h>
+#include <stdlib.h>
+#include <term.h>
+
+int main(void) {
+  char *p;
+
+  setupterm(NULL, 0, NULL);
+
+  p = tigetstr("cup");
+  p = tparm(p, 10, 20);
+
+  putp(p);
+
+  return 0;
+}
